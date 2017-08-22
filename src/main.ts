eceafebaf78cd5a5a94221d759ea963bbc6996da
Leonora Sanchez-Rees <leonora621@yahoo.com>
2017-08-21 20:59:40 -0600

    updated main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,7 @@
-// Angular 4
-import "@angular/platform-browser";
-import "@angular/platform-browser-dynamic";
-import "@angular/core";
-import "@angular/common";
-import "@angular/http";
-import "@angular/router";
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import { AppModule } from "./app/app.module";
+import { enableProdMode } from "@angular/core";
 
-// RxJS
-import "rxjs";
-
-// Other vendors for example Lodash or Bootstrap
-// You can import js, ts, css, sass, ...
-import "jquery";
-import "bootstrap-loader";
-import "font-awesome-webpack2";
+//only use when app is going live effects debugging
+enableProdMode();
+platformBrowserDynamic().bootstrapModule(AppModule);
